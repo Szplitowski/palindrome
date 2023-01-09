3a64517d355e1d06299eadfec879b80c80e507dc
Szplitowski <122147159+Szplitowski@users.noreply.github.com>
2023-01-09 13:12:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+def palin(word):
+    backw = ''.join(reversed(word))
+    if backw == word:
+        return True
+    return False
+    
+word = "kajak"
+yes = palin(word)
 
+if yes:
+    print("To słowo jest palindormem")
+else:
+    print("To słowo nie jest palindormem")
